54633f8971040edf471f811fad5510beb6059456
Ali <lisalmanov84@gmail.com>
2022-08-04 18:53:45 +0400

    taskk

---
+++ Homeework/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Homeework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 5, 2, 3, 4, 1, 6, };
+            Console.WriteLine(Min(arr));
+
+            
+        }
+        static int Min(int[] arr) 
+        {
+            int min = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (min > arr[i])
+                {
+                    min= arr[i];
+                }
+            }
+            return min;
+        }
+          
+    }
+}
